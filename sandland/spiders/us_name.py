e310af55f25a0c808fdb7473dd65b4dfed0c37d1
shenxuanfeng <shenxf_T_1000@163.com>
2018-01-09 17:24:43 +0800

    资料收集

--- sandland/spiders/us_name.py
+++ sandland/spiders/us_name.py
@@ -4,6 +4,8 @@ import scrapy
 
 # US'person name spider: 获取外国人的英文名,在一些注册中会使用到
 # 如何让爬虫爬取到一定数据,就暂停; 在暂停之后,如果有需要,怎么重新启动爬虫,但是有不会重新爬取数据
+# 主要目标网站: stackoverflow / facebook / yahoo ...
+# 具体参考: https://www.zhihu.com/question/20247711
 class UsNameSpider(scrapy.Spider):
     name = 'us_name'
     allowed_domains = ['https://www.facebook.com/facebook']
