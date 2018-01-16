d3f2469b2e1b310699912abbc06f0be8c41adb28
shenxuanfeng <shenxf_T_1000@163.com>
2018-01-16 17:07:39 +0800

    + api

--- sandland/spiders/a89ip_proxy.py
+++ sandland/spiders/a89ip_proxy.py
@@ -5,8 +5,8 @@ import scrapy
 # 89ip代理spider
 class A89ipProxySpider(scrapy.Spider):
     name = '89ip_proxy'
-    allowed_domains = ['http://www.89ip.cn/']
-    start_urls = ['http://http://www.89ip.cn//']
+    allowed_domains = ['www.89ip.cn/']
+    start_urls = ['http://www.89ip.cn/']
 
     custom_settings = {
         'DOWNLOAD_DELAY': 10,
@@ -14,7 +14,7 @@ class A89ipProxySpider(scrapy.Spider):
             'sandland.pipelines.ProxyIpPipeline': 1
         },
         'DOWNLOADER_MIDDLEWARES': {
-            #禁用默认的User-Agent middleware
+            # 禁用默认的User-Agent middleware
             'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
             'sandland.middlewares.RotateUserAgentMiddleware': 543
 
@@ -23,3 +23,9 @@ class A89ipProxySpider(scrapy.Spider):
 
     def parse(self, response):
         pass
+
+# http://www.89ip.cn/tiqv.php?sxb=&tqsl=30&ports=&ktip=&xl=on&submit=%CC%E1++%C8%A1
+# Request Method:GET
+
+
+
