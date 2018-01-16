a85a3a053d18dbd88ed7b33d15c07b78b14aefa5
shenxuanfeng <shenxf_T_1000@163.com>
2018-01-16 15:53:16 +0800

    + 快代理的免费代理爬取

--- sandland/spiders/quick_proxy.py
+++ sandland/spiders/quick_proxy.py
@@ -6,7 +6,7 @@ import scrapy
 class QuickProxySpider(scrapy.Spider):
     name = 'quick_proxy'
     allowed_domains = ['www.kuaidaili.com']
-    start_urls = ['http://www.kuaidaili.com/']
+    start_urls = ['https://www.kuaidaili.com/free/']
 
     custom_settings = {
         'DOWNLOAD_DELAY': 10,
@@ -22,4 +22,18 @@ class QuickProxySpider(scrapy.Spider):
     }
 
     def parse(self, response):
-        pass
+
+        proxies = response.xpath('//*[@id="list"]/table/tbody/tr')
+        if proxies:
+            for proxy in proxies:
+                ip = proxy.xpath('td[1]/text()').extract_first()
+                port = proxy.xpath('td[2]/text()').extract_first()
+                protocol = proxy.xpath('td[4]/text()').extract_first().lower()
+
+                yield {
+                    'proxy': '%s://%s:%s' % (protocol, ip, port)
+                }
+
+        next_page = response.xpath('//*[@id="listnav"]/ul/li/a[@class="active"]/../following-sibling::li[1]/a/@href').extract_first()
+        if next_page is not None:
+            yield response.follow(next_page, self.parse)
