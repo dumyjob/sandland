3277f80c38eed029ae837a8d872be7efff503f48
shenxuanfeng <shenxf_T_1000@163.com>
2018-01-19 17:18:43 +0800

    网易邮箱俱乐部 -- 自动签到

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 1. 各种彩票数据爬取,练手
 2. 爬取ed2k种子
 3. 爬取表情包,兴起
+    gif： https://segmentfault.com/q/1010000008651996
+    
 4. 爬取关系网(qq,weixin,weibo,知乎....)
 5. 用户上网数据(qq,weixin,momo...)
 6. 爬取用户小号(ip归属地来识别是否是同一人的)
@@ -11,3 +13,18 @@
 11. 返现类似的消息,做返现,推荐
 12. 注册邮箱(ZOHO...),并激活邮箱保存到其他介质中
 13. 网易邮箱签到活动,添加积分
+    网易邮箱俱乐部的签到,需要Login,是一个独立的Login界面, 可以得到请求的link,但是其中的数据不知道是
+    怎么处理出来的,在点击登录后. 对登录的信息做了一些混淆处理,加密等操作. 不太好找具体的加密等操作,成本
+    也太高.  scrapy.http.FormRequest.from_response 貌似可以处理这样的问题.
+    Link: http://scrapy-chs.readthedocs.io/zh_CN/1.0/topics/request-response.html#scrapy.http.FormRequest.from_response
+    
+    签到地址: http://club.mail.163.com/mission/task/signin.do?from=signinframe&random=0.408874460884626
+    (需要保留cookie)
+    click btn id :signinstate
+    
+14. https://segmentfault.com/ 可以尝试注册多个账号
+    1. 收取邮件,读取邮件内容,eg: 激活链接
+    2. proxy验证,先验证请求已经使用了proxy,不是本机ip了再发出请求
+    (proxy的稳定性是一个难题,如何确保稳定的ip代理池)
+    3. 如何绕过需要手机号验证的注册过程
+    4. +
--- sandland/spiders/email_club_auto_check_in.py
+++ sandland/spiders/email_club_auto_check_in.py
@@ -1,12 +1,57 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import random
+import json
+import time
 
 
-#
+# scrapy crawl email_club_auto_check_in
 class EmailClubAutoCheckInSpider(scrapy.Spider):
     name = 'email_club_auto_check_in'
-    allowed_domains = ['http://club.mail.163.com/jifen/index.do']
-    start_urls = ['http://http://club.mail.163.com/jifen/index.do/']
+    allowed_domains = ['club.mail.163.com']
+    start_urls = ['http://club.mail.163.com/jifen/index.do']
+
+    custom_settings = {
+        'DUPEFILTER_CLASS': 'scrapy.dupefilters.BaseDupeFilter'
+    }
 
     def parse(self, response):
-        pass
+        return scrapy.FormRequest.from_response(
+            response,
+            formid='login-form',
+            formdata={'email': 'shenxf_T_1000@163.com', 'password': 'shenxfT1000'},
+            callback=self.after_login
+        )
+
+    def after_login(self, response):
+        if "帐号或密码错误" in str(response.body):
+            self.log("Login failed")
+            return
+        else:
+            email = response.xpath('//div[@id="dvAccountLnk"]/text()').extract_first()
+            if email is not None:
+                self.log('%s, 登录成功' % email)
+
+            # 自动签到
+            # $("#signinstate").attr("class","btn btn-sign btn-sign-succ");
+            sign_success_link = response.xpath('//a[@id="signinstate"][contains(@class, "btn btn-sign btn-sign-succ")]').extract_first()
+            # response.xpath('//a[@id="signinstate"]')
+            if sign_success_link is not None:
+                # 已签到
+                self.log('%s,已签到.' % time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+                return
+
+            sign_req = '../mission/task/signin.do?from=signinframe&random=%s' % random.random()
+            yield scrapy.Request(url=response.urljoin(sign_req), callback=self.after_sign)
+
+    def after_sign(self, response):
+        result = json.loads(response.body)
+        if result['retCode'] == '200':
+            self.log('%s,签到成功.' % time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+        elif result['retCode'] == '401':
+            self.log('%s,已签到.' % time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+        else:
+            self.log('%s,签到失败.' % time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+
+        time.sleep(60 * 60 * 24)
+        yield response.follow(self.start_urls[0], callback=self.parse, dont_filter=True)
